9ede93b03e0b006b451a6e9c403e71c3000095fa
kevin4moe <kevin4moe@gmail.com>
2024-02-05 14:32:34 -0600

    header

--- pages/index.vue
+++ pages/index.vue
@@ -84,6 +84,16 @@ function upData() {
 </script>
 
 <template>
+  <header class="relative w-full min-h-40 overflow-hidden bg-[#5e1780] py-8">
+    <div class=" flex flex-col justify-around items-center">
+      <img class="w-1/6 pt-4 object-contain animate-spin-slow" src="/image/logo_pm_white.png" alt="">
+      <h4 class="pt-4 text-lg font-bold">PROYECTO MIGALA</h4>
+    </div>
+    <div class="absolute w-full h-full">
+      <div class="absolute -top-96 -left-64 w-[510px] h-[510px] bg-[#d13ce3] rounded-full opacity-30 z-10"></div>
+      <div class="absolute -top-72 -right-80 w-[410px] h-[410px] bg-[#a2be1d] rounded-full opacity-30 z-10"></div>
+    </div>
+  </header>
   <main class="container mx-auto">
     <UFormGroup label="¿Cómo te gustaría que nos refiramos a ti?" required>
       <UInput placeholder="Rick, Dai, Pepe, etc..." icon="i-heroicons-envelope" />
@@ -102,3 +112,19 @@ function upData() {
     </UFormGroup>
   </main>
 </template>
+
+<style>
+.animate-spin-slow {
+  animation: spin 7s linear infinite;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
