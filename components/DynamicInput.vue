9229fe90d15476970f31b1810d380b9839a887fd
kevin4moe <kevin4moe@gmail.com>
2024-02-25 21:21:35 -0600

    formulario 5-10

--- components/DynamicInput.vue
+++ components/DynamicInput.vue
@@ -1,9 +1,28 @@
 <script setup>
+import { watch } from 'vue'
+
 const state = defineModel('state')
 const props = defineProps({
   question: {
     type: Object,
   }
+});
+
+const checkbox = reactive([]);
+const checkList = reactive([]);
+
+if (props.question.type === 'checkbox') {
+  console.log(true)
+  props.question.choices.map((val, i) => {
+    checkbox[i] = {
+      name: val,
+      active: false
+    }
+  })
+}
+
+watch(checkbox, (newValue, oldValue) => {
+  console.log(newValue, oldValue);
 });
 
 const useChoices = (choices) => {
@@ -25,13 +44,14 @@ const icons = [
 <template>
   <UInputMenu v-if="question.type == 'radiogroup'" v-model="state" :options="useChoices(question.choices)"
     :icon="question.icon" />
-  <USelect v-else-if="question.type == 'dropdown'" :icon="question.icon" :options="question.choices"
-    :placeholder="question.example['es']" />
+  <USelect v-else-if="question.type == 'dropdown'" :options="question.choices" />
   <div v-else-if="question.type == 'panel'">
     <img :src="question.elements[0].imageLink" class="w-96" :alt="question.elements[0].name">
     <UInput :placeholder="question.elements[1].title['es']" :icon="question.elements[1].icon"
       :type="question.elements[1].type" v-model="state" />
   </div>
-  <UInput v-else="question.type != 'radiogroup'" :placeholder="question.example['es']" :icon="question.icon"
-    :type="question.inputType" v-model="state" />
+  <div v-else-if="question.type == 'checkbox'">
+    <UCheckbox v-for="(value, i) in question.choices" v-model="state" :label="value" />
+  </div>
+  <UInput v-else :placeholder="question.example['es']" :icon="question.icon" :type="question.inputType" v-model="state" />
 </template>
